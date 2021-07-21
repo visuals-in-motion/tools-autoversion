b4a6dfce29e34eb4c3f18c3a868567605fa4575d
Оля Набиева <olya-nabieva@mail.ru>
2021-07-21 18:24:54 +0300

    Init package

---
+++ Scripts/Editor/AutoVersion.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace Visuals
+{
+	public class AutoVersion
+	{
+		[DidReloadScripts]
+		private static void OnScriptsReloaded()
+		{
+			PlayerSettings.companyName = "Visuals";
+			PlayerSettings.runInBackground = true;
+			PlayerSettings.visibleInBackground = true;
+			PlayerSettings.SplashScreen.showUnityLogo = false;
+
+			string gitRepositoryName = CommandLine.Run("git config --get remote.origin.url");
+			string[] splitGitRepositoryName = gitRepositoryName.Split('/');
+			gitRepositoryName = splitGitRepositoryName[splitGitRepositoryName.Length - 1].Replace(".git", "");
+			gitRepositoryName = gitRepositoryName.Replace(Environment.NewLine, "");
+			gitRepositoryName = gitRepositoryName.Replace("\n", "");
+			PlayerSettings.productName = gitRepositoryName;
+
+			string gitCommitsCount = CommandLine.Run("git rev-list --count HEAD");
+
+			string[] versionArray = PlayerSettings.bundleVersion.Split('.');
+			string versionMajor = string.Empty;
+
+			if (versionArray.Length > 2)
+			{
+				for (int i = 0; i < versionArray.Length - 1; i++) { if (i < 2) { versionMajor += versionArray[i] + "."; } }
+			}
+			else if (versionArray.Length <= 2)
+			{
+				versionMajor = versionArray[0] + "." + versionArray[1] + ".";
+			}
+			else
+			{
+				versionMajor = "1.0.";
+			}
+
+			PlayerSettings.bundleVersion = versionMajor + gitCommitsCount;
+			PlayerSettings.macOS.buildNumber = PlayerSettings.bundleVersion;
+		}
+	}
+}
---
+++ Scripts/Runtime/Version.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Visuals
+{
+	public class Version : MonoBehaviour
+	{
+		[RuntimeInitializeOnLoadMethod]
+		private static void Show()
+		{
+			GameObject canvasVersion = new GameObject("CanvasVersion");
+			Canvas canvas = canvasVersion.AddComponent<Canvas>();
+			canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+			canvas.sortingOrder = 32000;
+			CanvasScaler canvasScaler = canvasVersion.AddComponent<CanvasScaler>();
+			canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+			canvasScaler.referenceResolution = new Vector2(1920, 1080);
+			canvasVersion.AddComponent<GraphicRaycaster>();
+
+			GameObject textVersion = new GameObject("TextVersion");
+			textVersion.transform.SetParent(canvasVersion.transform);
+
+			Text version = textVersion.AddComponent<Text>();
+			version.text = "Версия " + Application.version;
+			version.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+			version.fontStyle = FontStyle.Normal;
+			version.alignment = TextAnchor.LowerRight;
+			version.color = Color.red;
+
+			RectTransform rectTransformVersion = textVersion.GetComponent<RectTransform>();
+			rectTransformVersion.sizeDelta = new Vector2(120, 30);
+			rectTransformVersion.anchorMin = new Vector2(1, 0);
+			rectTransformVersion.anchorMax = new Vector2(1, 0);
+			rectTransformVersion.pivot = new Vector2(1, 0);
+			rectTransformVersion.anchoredPosition = new Vector2(-50, 50);
+
+			Object.Destroy(canvasVersion, 10);
+		}
+	}
+}
