d6da180925773dcc27df1b346a8c8e74e4f45098
Оля Набиева <olya-nabieva@mail.ru>
2021-07-22 12:25:11 +0300

    Автозагрузка пакета для работы с командной строкой

---
+++ Scripts/Editor/InstallDependency.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.IO;
+using System.Linq;
+
+namespace Visuals
+{
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+    public class InstallDependency
+    {
+        static InstallDependency()
+        {
+            string manifestPath = Path.GetFullPath("Packages/manifest.json");
+            string commandPackage = "    \"ru.visuals.command\": \"https://github.com/visuals-in-motion/tools-command.git\",";
+
+            List<string> file = File.ReadAllLines(manifestPath).ToList();
+
+            if (!file.Contains(commandPackage))
+            {
+                file.Insert(2, commandPackage);
+                File.WriteAllLines(manifestPath, file);
+            }
+        }
+    }
+#endif
+}
